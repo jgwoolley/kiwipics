1f15003f5c27682743172614513679867630177c
James W <26191568+jgwoolley@users.noreply.github.com>
2025-12-11 20:59:15 +0000

    added setup

---
+++ code-server/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install unzip
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 20
+nvm use 20
+npm install
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip ./aws
+
+# git log -1 --pretty=format:"%an <%ae>"
